ea403073660410e87b7b925035d8e9d556bb8b24
Dmytro Orlov <orlov.dm@gmail.com>
2019-12-23 18:38:54 +0300

    2019-12-23-18-38-54

--- src/main/scala/livecode/Hello.scala
+++ src/main/scala/livecode/Hello.scala
@@ -1,15 +1,29 @@
 package livecode
 
+import livecode.code.program
 import zio._
 import zio.console._
 
 object code {
+
+  case class Bot(name: String)
+
+  sealed trait Action
+
+  object EnterName extends Action
+
+  object SaveName extends Action
+
+  object ChoosePidorOfTheDay extends Action
+
+  object ChooseSweetCoupleOfTheDay extends Action
+
   val program =
     putStrLn("hello")
 }
 
 object Main extends App {
   def run(args: List[String]) =
-    code.program
+    program
       .as(0)
 }
